950d1895dee701061a3bd36f1982552189cf65ed
Becky McCarthy <beckymccarthy777@gmail.com>
2020-06-25 19:27:24 -0700

    initial commit

---
+++ pages/comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <h1>Comment Page</h1>
+        <Link href="/index">
+            <a>Home Page</a>
+        </Link>
+    </div>
+);
---
+++ pages/crypto/index.js
@@ -0,0 +1,5 @@
+export default () => (
+    <div>
+        <p>This is a crypto App!</p>
+    </div>
+);
